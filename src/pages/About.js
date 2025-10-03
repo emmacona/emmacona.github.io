96dbc76ff100232dd375d2d37710194a83c45d85
emmacona <emmanuelle@coutunadeau.com>
2025-10-03 16:13:46 -0400

    Description adjustements

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,15 +9,15 @@ function About() {
           <h1>About Me</h1>
         <p>
           Hi, I'm <span className="highlight">Emmanuelle Coutu-Nadeau</span>, a <span className="highlight">psychology master's student </span> 
-          specializing in the clinical applications of <span className="highlight">Artificial Intelligence </span> 
+          specializing in the clinical applications of <span className="highlight">artificial intelligence </span> 
           to identify neurological biomarkers of autism in EEG recordings.
           I have a background in <span className="highlight">software engineering</span> and experience in <span className="highlight">digital health</span> as a 
-          Software Developer and Project Manager at <span className="highlight">CareSimple</span>.
+          software developer and project manager at <span className="highlight">CareSimple</span>.
         </p>
         <p>
           My journey combines <span className="highlight">technology</span>, <span className="highlight">healthcare</span>, 
           and <span className="highlight">neuropsychology</span> to make meaningful discoveries and advancements in the field. 
-          I'm excited about the intersections of <span className="highlight">Artificial Intelligence</span> and <span className="highlight">mental health</span>, and aim to 
+          I'm excited about the intersections of <span className="highlight">artificial intelligence</span> and <span className="highlight">mental health</span>, and aim to 
           contribute to innovative solutions that benefit both research and clinical practices.
         </p>
         <div className="social-links">
