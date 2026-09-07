7e9455e4a9eea8d2783b0ffb6b79a58906dde2a4
emmacona <emmanuelle@coutunadeau.com>
2026-09-07 15:04:32 -0400

    Update CV

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,13 +10,13 @@ function About() {
         <p>
           Hi, I'm <span className="highlight">Emmanuelle Coutu-Nadeau</span>, a <span className="highlight">psychology master's student </span> 
           specializing in the clinical applications of <span className="highlight">artificial intelligence </span> 
-          to identify neurological biomarkers of autism in EEG recordings.
+          to identify neurphysiological biomarkers of autism in EEG recordings.
           I have a background in <span className="highlight">software engineering</span> and experience in <span className="highlight">digital health</span> as a 
           software developer and project manager at <span className="highlight">CareSimple</span>.
         </p>
         <p>
           My journey combines <span className="highlight">technology</span>, <span className="highlight">healthcare</span>, 
-          and <span className="highlight">neuropsychology</span> to make meaningful discoveries and advancements in the field. 
+          and <span className="highlight">psychology</span> to make meaningful discoveries and advancements in the field. 
           I'm excited about the intersections of <span className="highlight">artificial intelligence</span> and <span className="highlight">mental health</span>, and aim to 
           contribute to innovative solutions that benefit both research and clinical practices.
         </p>
--- src/pages/Publications.js
+++ src/pages/Publications.js
@@ -5,41 +5,69 @@ function Publications() {
   return (
     <div className="publications">
       <h1>Publications</h1>
-      
+
       <section>
-        <h2>Peer-reviewed Journal Article</h2>
-        <p>
-          Verner, Maria MSc; Sirois, Amanda BSc; <span className="highlight">Coutu-Nadeau, Emmanuelle BSc</span>; et al. The Association Between Perceived
-          Injustice and Opioid Craving in Patients With Chronic Pain. <em>Journal of Addiction Medicine</em>, July 21, 2022.
-        </p>
+        <h2>Peer-reviewed Journal Articles</h2>
+        <ul>
+          <li>
+            <span className="highlight">Coutu-Nadeau, E.</span>, Arutiunian, V., Martin, C.-O., Fontaine, V., Bélanger, A.-M., Jizi, K., Knoth, I. S., Hosein, A., Davoudi, S., Elsabbagh, M., Forgeot D’Arc, B., Jacquemont, S., &amp; Lippé, S. (under review). Behavioral Clustering and Resting-State EEG Signatures of Transdiagnostic Neurodevelopmental Profiles.{' '}
+            <a href="https://doi.org/10.21203/rs.3.rs-10692457/v1" target="_blank" rel="noopener noreferrer">
+              https://doi.org/10.21203/rs.3.rs-10692457/v1
+            </a>
+          </li>
+          <li>
+            Arutiunian, V., Davoudi, S., Knoth, S. I., <span className="highlight">Coutu-Nadeau, E.</span>, Desjardins, J., O’Reilly, C., Hosein, A., Gomez, G. B., Martin, C.-O., Fontaine, V. K., Bélanger, A. M., Hébert-Lalonde, N., Rouleau, G., D’Arc, B. F., Elsabbagh, M., Jacquemont, S., &amp; Lippé, S. (under review). Auditory Sustained Brain Activity Links Genomic Variation to Language in Neurodevelopmental Disorders.
+          </li>
+          <li>
+            Verner, M., Sirois, A., <span className="highlight">Coutu-Nadeau, E.</span>, Fournier, C., Carriere, J., &amp; Martel, M. O. (2022). The association between perceived injustice and opioid craving in patients with chronic pain. <em>Journal of Addiction Medicine</em>.{' '}
+            <a href="https://doi.org/10.1097/adm.0000000000001022" target="_blank" rel="noopener noreferrer">
+              https://doi.org/10.1097/adm.0000000000001022
+            </a>
+          </li>
+        </ul>
       </section>
 
       <section>
-        <h2>Published Abstracts/Conference Presentations</h2>
+        <h2>Published Abstracts and Conference Presentations</h2>
         <ul>
           <li>
-            Herrero Babiloni A *, Frimerman L, Fournier C, <span className="highlight">Coutu-Nadeau E</span>, Ross A, Lavigne GJ, Martel MO. Sleep quality and prescription opioid misuse: the potential mediating role of pain intensity, negative affect, and opioid craving. <em>Sleep Med</em> 2019, Vol. 64, S152-153.
+            <span className="highlight">Coutu-Nadeau, E.</span>, Arutiunian, V., Martin, C. O., Fontaine, V., Bélanger, A. M., Jizi, K., Knoth, I. S., Hosein, A., Davoudi, S., Elsabbagh, M., Forgeot D’Arc, B., Jacquemont, S., &amp; Lippé, S. Behavioral Clustering and Resting-state EEG Signatures of Neurodevelopmental Profiles Using Machine Learning. <em>Society for Psychophysiological Research</em>, Halifax, NS, Canada (October 2026, upcoming).
           </li>
           <li>
-            Frimerman L *, Fournier C, <span className="highlight">Coutu-Nadeau E</span>, Ross A. Martel MO. Determinants of prescription opioid craving and opioid-related hedonic feelings among patients with chronic pain prescribed opioid therapy. <em>The Journal of Pain</em> 2019 Vol. 20, Issue 4, S55–S56.
+            <span className="highlight">Coutu-Nadeau, E.</span>, Arutiunian, V., Martin, C. O., Fontaine, V., Bélanger, A. M., Jizi, K., Knoth, I. S., Hosein, A., Davoudi, S., Elsabbagh, M., Forgeot D’Arc, B., Jacquemont, S., &amp; Lippé, S. Behavioral Clustering and Resting-state EEG Signatures of Neurodevelopmental Profiles Using Machine Learning. <em>Child Neurology Society</em>, Montreal, QC, Canada (October 2026, upcoming).
           </li>
           <li>
-            Herrero Babiloni A *, Frimerman L, Fournier C, <span className="highlight">Coutu-Nadeau E</span>, Ross A, Lavigne G, Martel MO. Sleep quality and opioid craving among patients with chronic pain and prescribed opioid therapy: explorations of potential mediators. <em>11th Congress of European Pain Federation</em>, Valencia, Spain. (September 2019).
+            <span className="highlight">Coutu-Nadeau, E.</span>, Arutiunian, V., Martin, C. O., Fontaine, V., Bélanger, A. M., Jizi, K., Knoth, I. S., Hosein, A., Davoudi, S., Elsabbagh, M., Forgeot D’Arc, B., Jacquemont, S., &amp; Lippé, S. Behavioral Clustering and Resting-state EEG Signatures of Neurodevelopmental Profiles Using Machine Learning. <em>93e Congrès de l’Acfas (TACC Trainee Research Day on Autism and Neurodivergence)</em>, Trois-Rivières, QC, Canada (May 12, 2026).
           </li>
           <li>
-            Herrero Babiloni A *, Frimerman L, Fournier C, <span className="highlight">Coutu-Nadeau E</span>, Ross A, Lavigne GJ, Martel MO. Sleep quality and prescription opioid misuse: the potential mediating role of pain intensity, negative affect, and opioid craving. <em>World Sleep Congress</em>, Vancouver, BC, Canada (September 2019).
+            <span className="highlight">Coutu-Nadeau, E.</span>, Arutiunian, V., Martin, C. O., Fontaine, V., Bélanger, A. M., Jizi, K., Knoth, I. S., Hosein, A., Davoudi, S., O’Reilly, C., Rouleau, G., Ernst, C., Lucas-Tardif, C., Scorah, J., Couture, M., Abadie, P., Joober, R., Forgeot d’Arc, B., Jacquemont, S., Elsabbagh, M., &amp; Lippé, S. Resting-state EEG signatures of ASD/ADHD: toward identifying behavioral subgroups. <em>Society for Psychophysiological Research Annual Meeting</em>, Montreal, QC, Canada (October 2025).
           </li>
           <li>
-            Frimerman L *, Fournier C, <span className="highlight">Coutu-Nadeau E</span>, Ross A. Martel MO. Determinants of prescription opioid craving and opioid-related hedonic feelings among patients with chronic pain prescribed opioid therapy. <em>23rd Annual McGill Pain Day</em>, Montreal, Quebec, Canada (January 2019).
+            Kennedy, G., Baran-Goldwax, M., Pomerleau, R., <span className="highlight">Coutu-Nadeau, E.</span>, Knoth, I. S., &amp; Lippé, S. Hormonal modulation of brain dynamics in menstruating adolescents and young adults: EEG insights across the menstrual cycle. <em>Society for Psychophysiological Research Annual Meeting</em>, Montreal, QC, Canada (October 2025).
           </li>
           <li>
-            Frimerman L *, Fournier C, <span className="highlight">Coutu-Nadeau E</span>, Ross A. Martel MO. Determinants of prescription opioid craving and opioid-related hedonic feelings among patients with chronic pain prescribed opioid therapy. <em>38th Annual Scientific Meeting of the American Pain Society</em> (APS), Milwaukee, WI, USA (April 2019).
+            Kennedy, G., Baran-Goldwax, M., <span className="highlight">Coutu-Nadeau, E.</span>, Pomerleau, R., Knoth, I. S., &amp; Lippé, S. Feeling everything: estrogen, cortisol, and sensory brain dynamics across the menstrual cycle. <em>International Society of Psychoneuroendocrinology Annual Conference</em>, New Orleans, LA, USA (September 2025).
           </li>
           <li>
-            Ouellet N *, Fournier C, Frimerman L, <span className="highlight">Coutu-Nadeau E</span>, Martel MO. Factors associated with opioid craving among patients with chronic pain prescribed long-term opioid therapy. <em>14th Annual Research Day of McGill’s Faculty of Dentistry</em>, Montreal, Quebec Canada. (April 2019).
+            Herrero Babiloni, A., Frimerman, L., Fournier, C., <span className="highlight">Coutu-Nadeau, E.</span>, Ross, A., Lavigne, G. J., &amp; Martel, M. O. Sleep quality and prescription opioid misuse: the potential mediating role of pain intensity, negative affect, and opioid craving. <em>Sleep Medicine</em>, 2019, Vol. 64, S152–S153.
           </li>
           <li>
-            Frimerman L *, Fournier C, <span className="highlight">Coutu-Nadeau E</span>, Ross A. Martel MO. Determinants of Prescription Opioid Craving and Opioid-related Hedonic Feelings Among Patients with Chronic Pain Prescribed Opioid Therapy. <em>19th Annual McGill Biomedical Graduate Conference</em>, Montreal, Quebec, Canada. (March 2019).
+            Frimerman, L., Fournier, C., <span className="highlight">Coutu-Nadeau, E.</span>, Ross, A., &amp; Martel, M. O. Determinants of prescription opioid craving and opioid-related hedonic feelings among patients with chronic pain prescribed opioid therapy. <em>The Journal of Pain</em>, 2019, Vol. 20, Issue 4, S55–S56.
+          </li>
+          <li>
+            Herrero Babiloni, A., Frimerman, L., Fournier, C., <span className="highlight">Coutu-Nadeau, E.</span>, Ross, A., Lavigne, G. J., &amp; Martel, M. O. Sleep quality and opioid craving among patients with chronic pain and prescribed opioid therapy: explorations of potential mediators. <em>11th Congress of the European Pain Federation</em>, Valencia, Spain (September 2019).
+          </li>
+          <li>
+            Herrero Babiloni, A., Frimerman, L., Fournier, C., <span className="highlight">Coutu-Nadeau, E.</span>, Ross, A., Lavigne, G. J., &amp; Martel, M. O. Sleep quality and prescription opioid misuse: the potential mediating role of pain intensity, negative affect, and opioid craving. <em>World Sleep Congress</em>, Vancouver, BC, Canada (September 2019).
+          </li>
+          <li>
+            Frimerman, L., Fournier, C., <span className="highlight">Coutu-Nadeau, E.</span>, Ross, A., &amp; Martel, M. O. Determinants of prescription opioid craving and opioid-related hedonic feelings among patients with chronic pain prescribed opioid therapy. <em>23rd Annual McGill Pain Day</em>, Montreal, QC, Canada (January 2019).
+          </li>
+          <li>
+            Frimerman, L., Fournier, C., <span className="highlight">Coutu-Nadeau, E.</span>, Ross, A., &amp; Martel, M. O. Determinants of prescription opioid craving and opioid-related hedonic feelings among patients with chronic pain prescribed opioid therapy. <em>38th Annual Scientific Meeting of the American Pain Society</em> (APS), Milwaukee, WI, USA (April 2019).
+          </li>
+          <li>
+            Ouellet, N., Fournier, C., Frimerman, L., <span className="highlight">Coutu-Nadeau, E.</span>, &amp; Martel, M. O. Factors associated with opioid craving among patients with chronic pain prescribed long-term opioid therapy. <em>14th Annual Research Day of McGill’s Faculty of Dentistry</em>, Montreal, QC, Canada (April 2019).
           </li>
         </ul>
       </section>
